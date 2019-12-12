a7c0c283ace6ea989f4a95e266c174e291a46409
danielvu1994 <vulinhdam1994@gmail.com>
2019-12-12 22:51:33 +0700

    2019/12/12 - Commit10 - Broadcast Comment

--- lib/discuss2_web/channels/comments_channel.ex
+++ lib/discuss2_web/channels/comments_channel.ex
@@ -9,7 +9,7 @@ defmodule Discuss2.CommentsChannel do
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
     topic = Discussions.get_topic!(topic_id)
-    IO.inspect(topic) 
+    IO.inspect(topic)
 
     {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
@@ -25,7 +25,15 @@ defmodule Discuss2.CommentsChannel do
     #   |> Comment.changeset(%{content: content})
 
     # IO.inspect(changeset)
-    Discussions.create_comment(%{"content" => content}, topic)
-    {:reply, :ok, socket}
+    case Discussions.create_comment(%{"content" => content}, topic) do
+      {:ok, comment} ->
+        # Notify all user about a comment is added
+          broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+            %{comment: comment}
+          )
+        {:reply, :ok, socket}
+      {:error, reason} ->
+         {:reply, {:error, %{errors: content}}, socket}
+    end
   end
 end
